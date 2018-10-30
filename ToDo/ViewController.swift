c4ea3364f7637038f8b4bec7c9bdb1f51428f815
Milan Bojic <milan.bojic.developer@gmail.com>
2018-10-30 14:17:10 +0100

    UITableViewController set

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -4,17 +4,28 @@
 //
 //  Created by Milan Bojic on 10/30/18.
 //  Copyright © 2018 Milan Bojic. All rights reserved.
-//
+
+// UITableViewController has all the delegates, protocols set by default
 
 import UIKit
 
-class ViewController: UIViewController {
+class ToDoListVC: UITableViewController {
 
+    let itemArray = ["Some cell", "Another text", "Third word"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 
 }
 
